86cc469dd69cb6b6f2579dc1441e2dd722cab67b
Todd A. Oliver <oliver@ices.utexas.edu>
2018-02-12 13:18:25 -0600

    Skeleton for testing hybrid rdelta using boost unit test based framework

---
+++ SU2_CFD/test/hybrid_rdelta_test.cpp
@@ -0,0 +1,76 @@
+/*!
+ * \file resolution_integration_test.cpp
+ * \brief This test checks whether the resolution tensor is correctly set for a grid
+ * of quadrilateral cells.
+ * \author C. Pederson
+ * \version 4.3.0 "Cardinal"
+ *
+ * SU2 Lead Developers: Dr. Francisco Palacios (Francisco.D.Palacios@boeing.com).
+ *                      Dr. Thomas D. Economon (economon@stanford.edu).
+ *
+ * SU2 Developers: Prof. Juan J. Alonso's group at Stanford University.
+ *                 Prof. Piero Colonna's group at Delft University of Technology.
+ *                 Prof. Nicolas R. Gauger's group at Kaiserslautern University of Technology.
+ *                 Prof. Alberto Guardone's group at Polytechnic University of Milan.
+ *                 Prof. Rafael Palacios' group at Imperial College London.
+ *                 Prof. Edwin van der Weide's group at the University of Twente.
+ *                 Prof. Vincent Terrapon's group at the University of Liege.
+ *
+ * Copyright (C) 2012-2016 SU2, the open-source CFD code.
+ *
+ * SU2 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * SU2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SU2. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define BOOST_TEST_MODULE hybrid_rdelta
+#include "MPI_global_fixture.hpp"
+
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <limits> // used to find machine epsilon
+#include <cmath>  // std::abs
+
+#include "../include/hybrid_RANS_LES_model.hpp"
+
+
+/* ----------------------------------------------------------------------------
+ *  Test Fixtures
+ * --------------------------------------------------------------------------*/
+
+struct HybridRdeltaFixture {
+  HybridRdeltaFixture()
+    : machine_eps(std::numeric_limits<su2double>::epsilon()) { }
+
+  ~HybridRdeltaFixture() {
+  }
+
+  const su2double machine_eps;
+};
+
+/* ----------------------------------------------------------------------------
+ *  Tests
+ * --------------------------------------------------------------------------*/
+
+#ifdef BUILD_TESTS
+
+BOOST_GLOBAL_FIXTURE( MPIGlobalFixture );
+
+BOOST_FIXTURE_TEST_CASE(ZeroGradientTrivial, HybridRdeltaFixture) {
+
+  BOOST_TEST_MESSAGE( "Running ZeroGradientTrival test..." );
+  BOOST_CHECK_EQUAL(1,1);
+
+}
+
+#endif
