ab39ef46ce1ec0d0d71c7c4302eb8f7bba13a7f1
Clark Pederson <clarkp@utexas.edu>
2019-04-18 15:04:41 -0500

    Add limits to improve convergence while transient
    This is a set of two related changes to the numerics, in order to
    improve stability.  The first limits the value of C_e1 to values that
    are realistic (and not near-infinite).  The second ensures that the
    "min" limit on the production of v2 is applied correctly.  If some terms
    are negative, then "min" has the opposite of the intended effect.
    
    Both of these changes have been observed to not harm convergence for an
    incompressible flat-plate, and they improve convergence for a M=2 flat
    plate test-case.

--- SU2_CFD/src/numerics_direct_turbulent_v2f.cpp
+++ SU2_CFD/src/numerics_direct_turbulent_v2f.cpp
@@ -309,8 +309,11 @@ void CSourcePieceWise_TurbKE::ComputeResidual(su2double *val_residual,
     VelMag += VelInf[iDim]*VelInf[iDim];
   VelMag = sqrt(VelMag);
 
-  const su2double tke_lim = max(tke, scale*VelMag*VelMag);
-  const su2double tdr_lim = max(tdr, scale*VelMag*VelMag*VelMag/L_inf);
+  const su2double TKE_MIN = scale*VelMag*VelMag;
+  const su2double TDR_MIN = scale*VelMag*VelMag*VelMag/L_inf;
+  const su2double tke_lim = max(tke, TKE_MIN);
+  const su2double v2_lim = max(v20, TKE_MIN);
+  const su2double tdr_lim = max(tdr, TDR_MIN);
 
   // make sure v2 is well-behaved
   su2double zeta = max(v20/tke_lim, scale);
@@ -392,8 +395,17 @@ void CSourcePieceWise_TurbKE::ComputeResidual(su2double *val_residual,
   su2double Pe, Pe_rk, Pe_re, Pe_rv2;
   su2double De, De_rk, De_re, De_rv2;
 
-  // NB: C_e1 depends on tke and v2 in v2-f
-  const su2double C_e1 = C_e1o*(1.0+0.045*sqrt(1.0/zeta));
+  /*---  C_e1 depends on tke and v2 in v2-f
+   * When v2 goes near zero during messy transient startups, C_e1 can grow
+   * very large. This can cause an imbalance, where dissipation is produced
+   * faster than it can be destroyed. This v2 dependence was originally
+   * intended to blend between C_e1 = 1.3 and C_e1 = 1.55.  So the variations
+   * should *not* be large, and should be clipped. As y+ -> 0,
+   * C_e1 -> infinity. But for channel flow at Re_tau = 5200, C_e1 < 29 for
+   * all y+ > .07. So we can set a relatively low limit (e.g. 100) to enforce
+   * our desired behavior. This should not affect converged solution. ---*/
+  const su2double inv_zeta = max(tke/v2_lim, 0.5);
+  const su2double C_e1 = min(C_e1o*(1.0+0.05*sqrt(inv_zeta)), 100.0);
 
   // ... production
   Pe = C_e1*Pk/TurbT;
@@ -416,7 +428,11 @@ void CSourcePieceWise_TurbKE::ComputeResidual(su2double *val_residual,
 
   // ... production
   // Limit production of v2 based on max zeta = 2/3
-  Pv2 = rho * min( tke*f, 2.0*Pk/3.0/rho + 5.0*v2/T1 );
+  if (Pk > 0 && v2/T1 > TDR_MIN) {
+    Pv2 = rho * min( tke*f, 2.0*Pk/3.0/rho + 5.0*v2/T1 );
+  } else {
+    Pv2 = rho * tke * f;
+  }
 
   Pv2_rk  = 0.0;
   Pv2_re  = 0.0;
